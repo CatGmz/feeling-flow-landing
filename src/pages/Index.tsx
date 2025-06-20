4228872b7e90702d4e843c860a3774173220ab07
kellysolanomt <ksolanoc@unal.edu.co>
2025-06-20 07:38:04 -0500

    Prueba de push

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,10 +4,12 @@ import KitSection from "@/components/KitSection";
 import CommunitySection from "@/components/CommunitySection";
 import QuoteSection from "@/components/QuoteSection";
 import SubscriptionSection from "@/components/SubscriptionSection";
+import Navbar from "@/components/navbar";
 
 const Index = () => {
   return (
     <div className="min-h-screen">
+      <Navbar />
       <HeroSection />
       <KitSection />
       <CommunitySection />
