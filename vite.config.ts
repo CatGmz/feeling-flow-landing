8173d5280b01c54d0781ccbb4eb3db4caf0ccdf8
kellysolanomt <ksolanoc@unal.edu.co>
2025-06-23 17:52:04 -0500

    correccion

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/feeling-flow-landing/',
   server: {
     host: "::",
     port: 8080,
