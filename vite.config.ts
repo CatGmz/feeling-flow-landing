512b923b1760a368199a1bde41fd2440b4955015
kellysolanomt <ksolanoc@unal.edu.co>
2025-06-23 20:53:50 -0500

    prueba 4

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: 'https://catgmz.github.io/feeling-flow-landing',
+  base: '/feeling-flow-landing',
   server: {
     host: "::",
     port: 8080,
